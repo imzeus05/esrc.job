1263801ed0f3ebe7a56de1765049c6ef265f71d6
imzeus05 <imzeus05@gmail.com>
2017-03-16 13:44:13 +0900

    add control

--- ESRC.JOB/pageWeb.xaml.cs
+++ ESRC.JOB/pageWeb.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using mshtml;
+using System.Text.RegularExpressions;
+
+
 namespace ESRC.JOB
 {
     /// <summary>
@@ -29,16 +33,67 @@ namespace ESRC.JOB
         {
             Application.Current.MainWindow.WindowState = WindowState.Maximized;
             myBrowser.Navigate(new Uri("http://www.social-eq.co.kr/movie/login.do"));
+
         }
 
         private void myBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
         {
-            MessageBox.Show("현재 URL : " + e.Uri.ToString());
-
+            //MessageBox.Show("현재 URL : " + e.Uri.ToString());
+                        
             if (e.Uri.ToString() == "http://www.social-eq.co.kr/movie/testft_start.do")
             {
                 NavigationService.Navigate(new Uri("pageLocal.xaml", UriKind.Relative));
             }
         }
+
+        private void myBrowser_Navigated(object sender, NavigationEventArgs e)
+        {
+            //mshtml.IHTMLDocument3 document = (mshtml.IHTMLDocument3)myBrowser.Document;
+            //var user_email = document.getElementsByName("login_email")
+            //                .OfType<IHTMLElement>()
+            //                .Select(element => element.getAttribute("value"))
+            //                .FirstOrDefault();
+
+            //MessageBox.Show("현재 사용자 : " + user_email);
+
+
+            //var document = myBrowser.Document as mshtml.HTMLDocument;
+            //var inputs = document.getElementsByTagName("input");
+            //MessageBox.Show(document.title);
+            
+            
+                        
+         
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            // ... Create a new BitmapImage.
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri(@"img\ppg.jpg", UriKind.Relative);
+            b.EndInit();
+
+            // ... Get Image reference from sender.
+            var image = sender as Image;
+            // ... Assign Source.
+            image.Source = b;
+        }
+
+        private void Image_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            // ... Create a new BitmapImage.
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri(@"img\cam.jpg", UriKind.Relative);
+            b.EndInit();
+
+            // ... Get Image reference from sender.
+            var image = sender as Image;
+            // ... Assign Source.
+            image.Source = b;
+        }
+
+        
     }
 }
